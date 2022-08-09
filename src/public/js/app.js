a07ffd52536c6df8a9e343d5e638f9bca464a49b
jiyoung <higomn05@naver.com>
2022-08-09 22:13:58 +0900

    websocket message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-const socket = new WebSocket(`ws://${window.location.host}`)  // 여기서 socket은 서버로의 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data)
+})
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌")
+})
+
+
+setTimeout(() => {
+  socket.send("hello from the browser!")
+}, 10000)
--- src/server.js
+++ src/server.js
@@ -19,10 +19,15 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) { // 여기서 socket은 연결된 브라우저 의미
-  console.log(socket)
-}
-
-wss.on("connection", handleConnection)
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => {
+    console.log("Disconnected from Browser ❌")
+  })
+  socket.on("message", (message) => {
+    console.log(message.toString());
+  })
+  socket.send("hello!!");
+})
 
 server.listen(3000, handleListen)
