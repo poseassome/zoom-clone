aa965bf94a77211bbde5480ef1e6b303b8804abc
jiyoung <higomn05@naver.com>
2022-08-09 21:44:53 +0900

    ws server 생성

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -6,9 +8,15 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"))
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"))
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// app.listen(3000, handleListen);
 
-app.listen(3000, handleListen);
+
+/**  server에 httmp, ws 둘 다 동작  **/
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server })
+
+server.listen(3000, handleListen)
