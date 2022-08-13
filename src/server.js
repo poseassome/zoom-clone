c0ecbda9ec2ad38e4f5902304cd213524167226f
jiyoung <higomn05@naver.com>
2022-08-13 14:20:25 +0900

    nickname설정, chat 기능

--- src/server.js
+++ src/server.js
@@ -19,15 +19,43 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+function onSocketClose() {
+  console.log("Disconnected from Browser ❌");
+}
+
+/* fake Database */
+const sockets = []; //  <-- 접속하는 브라우저의 정보를 담음
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket['nickname'] = 'Anon';
   console.log("Connected to Browser ✅");
-  socket.on("close", () => {
-    console.log("Disconnected from Browser ❌")
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    // if (message.type === 'new_message') sockets.forEach((aSocket) => aSocket.send(message.payload));
+    // else if (message.type === 'nickname') console.log(message.payload);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case "nickname":
+        socket['nickname'] = message.payload;
+        break;
+    }
   })
-  socket.on("message", (message) => {
-    console.log(message.toString());
-  })
-  socket.send("hello!!");
 })
 
-server.listen(3000, handleListen)
+server.listen(3000, handleListen);
+
+/* type 정해주기
+
+  {
+    type: 'message',
+    payload: 'hello'
+  }
+  {
+    type: 'nickname',
+    payload: 'nick'
+  }
+                    */
